a93fce84f27829c5d839d35557f3a8eb2b2de6e6
jchoi3025 <jchoi3025@gmail.com>
2019-08-01 21:24:36 -0400

    HW

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
+
+function getData (){
+
+    var input = $("#searchtext").val()
+
 var xhr = $.get("http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=v0WEkxThNdvtMOl3xRWgoeEpjAi7WhTD&limit=30");
 
-xhr.done(function(data) {console.log("success got data", data); });
+xhr.done(function(response) 
+{console.log("success got data", response);
 
-var jiffs = response
+var jiffs = response.data
 
-for (i in jiffs) {
-    $('.inner').append("<img src='"+jiffs[i].imnages.original.url"' style='height:350px; width:350px;'/>")
+for (i in jiffs) 
+{
+    $('.inner').append("<img src='"+jiffs[i].imnages.original.url+"' style='height:350px; width:350px;'/>")
+
+}
+
+});
 
 }
